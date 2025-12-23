46e520bc8e67c67719d9a1def48df2868c769169
가현 <bufar1014@gmail.com>
2025-12-24 00:49:20 +0900

    위치 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,18 @@ class _MyAppState extends State<MyApp> {
       home : DefaultTabController(
         length : 5,
         child : Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Icon(Icons.my_location_sharp, size: 28, color: Colors.white),
+                SizedBox(width: 10),
+                // 아이콘 옆 '현재 위치'의 날씨는 '맑음 25도' 이런 식으로 표시
+              ],
+            ),
+            backgroundColor: Color(0xFF0B3D91),
+            elevation: 5,
+            centerTitle: false,
+          ),
           body : TabBarView(
             physics: NeverScrollableScrollPhysics(),
             children : <Widget> [
